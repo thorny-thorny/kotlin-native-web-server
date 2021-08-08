a054bc2a26f7fed888bdb2c440412ab30f7b7ccc
Thorny <35000252+thorny-thorny@users.noreply.github.com>
2021-08-08 13:52:59 +0700

    Gradle: native target auto selection

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,16 @@ repositories {
 }
 
 kotlin {
-  // TODO: linuxX64("native"), mingwX64("native")
-  macosX64("native") {
+  val hostOs = System.getProperty("os.name")
+  val isMingwX64 = hostOs.startsWith("Windows")
+  val nativeTarget = when {
+    hostOs == "Mac OS X" -> macosX64("native")
+    hostOs == "Linux" -> linuxX64("native")
+    isMingwX64 -> mingwX64("native") // TODO: this target
+    else -> throw GradleException("Host OS \"$hostOs\" is not supported (list of supported: MacOS, Linux, Windows; 64bit only)")
+  }
+
+  nativeTarget.apply {
     binaries {
       executable {
         entryPoint = "main"
